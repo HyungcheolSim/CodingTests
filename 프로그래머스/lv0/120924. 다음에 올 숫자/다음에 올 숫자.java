66d2f7a3dc350ff0ffba6e3ff6419879f41c6432
형철 심 <sim34122@naver.com>
2023-07-10 16:32:17 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,13 +1,6 @@
 class Solution {
     public int solution(int[] c) {
-        if(c[c.length-1]-c[c.length-2]==c[c.length-2]-c[c.length-3]){
-            //등차
-            int diff=c[c.length-1]-c[c.length-2];
-            return c[c.length-1]+diff;
-        }else{
-            //등비
-            int diff=c[c.length-1]/c[c.length-2];
-            return c[c.length-1]*diff;
-        }
+        return (c[c.length-1]-c[c.length-2]==c[c.length-2]-c[c.length-3])?
+             c[c.length-1]+(c[c.length-1]-c[c.length-2]): c[c.length-1]*(c[c.length-1]/c[c.length-2]);
     }
 }
