d6e53b22a14b0ab2a62713807560d19707b92626
형철 심 <sim34122@naver.com>
2023-07-12 11:07:00 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.07 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,8 +1,20 @@
 import java.util.*;
 class Solution {
     public long[] solution(long n) {
-        //n의 자리수를 반복문으로 파악하고 배열의 크기를 정하는 게 더 좋을까 아니면
-        //ArrayList로 하는게 좋을까 ->이걸로 해봄
+        //1. n의 자리수를 반복문으로 파악하고 배열의 크기를 정하는 게 더 좋을까 아니면
+//         String a = String.valueOf(n);
+//         int[] answer = new int[a.length()];
+
+//         for(int i = 0, j = a.length() - 1; i < a.length(); i++, j--) {
+//             answer[i] = ((int) a.charAt(j) - '0');
+//         }
+
+
+
+
+//         return answer;
+//     }
+        //2. ArrayList로 하는게 좋을까 ->이걸로 해봄
         List<Long> list=new ArrayList<>();
         while(n>0){
             list.add(n%10);
@@ -15,4 +27,6 @@ class Solution {
         return arr;
         //return list.stream().mapToInt(i->i).toArray();
     }
+    //3. stringbuilder best answer
+    //return new StringBuilder().append(n).reverse().chars().map(Character::getNumericValue).toArray();
 }
