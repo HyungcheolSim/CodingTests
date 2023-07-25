c7e4a09c3855e6465e02942183ce180799c7e3f1
형철 심 <sim34122@naver.com>
2023-07-25 11:51:30 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.13 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -1,11 +1,19 @@
 class Solution {
     public long solution(int a, int b) {
         long sum=0;
-        long max=a>b?a:b;
-        long min= max==a?b:a;
-        for(long i=min;i<=max;i++){
+        for (int i = ((a < b) ? a : b); i <= ((a < b) ? b : a); i++) {
             sum+=i;
         }
         return sum;
     }
+    /*
+        등차수열의 일반항 첫째항+구하는 항이 몇번째인지(n)-1*차수(d)
+        여기서는 차수 1 
+        등차수열의 합
+        마지막 항(l) = an=a+(n-1)d
+        첫째항이 a, 마지막 행이 l일 때 
+        첫째항이 a, 공차가 d일 때, n(2a+(n-1)d)/2
+        (b - a + 1) * (a + b) / 2;
+        첫째항부터 마지막행까지 합=Sn=n(a+l)/2
+    */
 }
