fb53a5629239c966415447dd70e933a84f7ed57a
형철 심 <sim34122@naver.com>
2023-07-26 11:09:50 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 2.04 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,10 +1,11 @@
 class Solution {
     public String solution(String phone_number) {
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<phone_number.length()-4;i++){
-            sb.append("*");
-        }
-        sb.append(phone_number.substring(phone_number.length()-4));
-        return sb.toString();
+        // StringBuilder sb=new StringBuilder();
+        // for(int i=0;i<phone_number.length()-4;i++){
+        //     sb.append("*");
+        // }
+        // sb.append(phone_number.substring(phone_number.length()-4));
+        // return sb.toString();
+        return phone_number.substring(0,phone_number.length()-4).replaceAll(".","*")+phone_number.substring(phone_number.length()-4);
     }
 }
