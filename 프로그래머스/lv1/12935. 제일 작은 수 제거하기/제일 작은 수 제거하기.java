bec218d20b12e9b636fd9346e1d85bf6e894a694
형철 심 <sim34122@naver.com>
2023-08-04 21:35:08 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.37 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        //정답 배열이 빈 배열인 경우 -1 리턴
+        int[] answer;
+        List<Integer> list= new ArrayList<>();
+        if(arr.length==1){
+            return new int[] {-1};
+        }
+        //최소값 구하기
+        int min=arr[arr.length-1];
+        for(int i=0;i<arr.length;i++){
+            if(min>arr[i]){
+                min=arr[i];
+            }
+        }
+        //새 arraylist에 넣기
+        for(int i:arr){
+            if(i==min)
+                continue;
+            list.add(i);
+        }
+        //arraylist 배열로 변환
+        return list.stream()
+                .mapToInt(i -> i)
+                .toArray();
+        
+    }
+}
