8c7f91430eb3b13ea923282aa9bdf8678ca8b147
형철 심 <sim34122@naver.com>
2023-08-22 22:20:19 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -1,21 +1,33 @@
 class Solution {
     public int solution(int n) {
-        int answer=0;
-        if(n<2){
-            return 0;
-        }else{
-            for(int i=2;i<=n;i++){
-                int count=0;
-                for(int j=1;j<=i;j++){
-                    if(i%j==0){
-                        count++;
+        int answer = 0;
+        for(int i=4; i<n+1; i++){
+            if(i%2==0) {
+                answer++; // 4이상 짝수는 전부 합성수
+                continue;
+            }
+            else{
+                for(int k=3; k<i/2; k=k+2){
+                    if(i%k==0){
+                        answer++;
+                        break;
                     }
                 }
-                if(count>=3)
-                    answer++;
-            }    
+            }
         }
-        
         return answer;
     }
 }
+    //     //4~n까지
+    //     int count=0;
+    //     for(int i=4;i<=n;i++){
+    //         //약수 구하기 j는 1부터 i까지
+    //         int num=0;
+    //         for(int j=1;j<=i;j++){
+    //             if(i%j==0) num++;
+    //         }
+    //         if(num>=3) count++;
+    //     }
+    //     return count;
+    // }
+//}
