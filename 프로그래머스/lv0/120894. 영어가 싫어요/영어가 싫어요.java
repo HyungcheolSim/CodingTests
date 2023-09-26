3eb1f7bd484e0f47cf528dd76969802b805138b8
형철 심 <sim34122@naver.com>
2023-09-26 11:44:14 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.10 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,12 +1,11 @@
 class Solution {
     public Long solution(String numbers) {
-        String[] strArr={"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-        for(int i=0;i< strArr.length;i++){
-            if(numbers.contains(strArr[i])){
-                numbers=numbers.replaceAll(strArr[i],String.valueOf(i));
+        String[] arr= {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i=0;i<arr.length;i++){
+            if(numbers.contains(arr[i])){
+                numbers=numbers.replace(arr[i],String.valueOf(i));
             }
         }
-        Long answer=Long.parseLong(numbers);
-        return answer;
+        return Long.parseLong(numbers);
     }
 }
