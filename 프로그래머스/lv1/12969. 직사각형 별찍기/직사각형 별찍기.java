b8d001cc9963c25aef8862ec718a161d87e697a4
형철 심 <sim34122@naver.com>
2023-09-30 09:58:00 +0900

    [level 1] Title: 직사각형 별찍기, Time: 162.90 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.java
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -6,11 +6,12 @@ class Solution {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        for(int i=0;i<b;i++){
-            for(int j=0;j<a;j++){
-                System.out.print("*");
-            }
-            System.out.println();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<a; i++){
+            sb.append("*");
+        }
+        for(int i=0; i<b; i++){
+            System.out.println(sb.toString());
         }
     }
 }
