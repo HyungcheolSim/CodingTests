3460e8c77a7fcb15f73fb984cdd67e200f254d86
형철 심 <sim34122@naver.com>
2023-10-04 10:20:35 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,22 +1,20 @@
 import java.util.*;
 class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
-        //기약분수 만들기
-        for(int i=2;i<=a;i++){ //2~a까지 나눠지는 수로 나눔
+        for(int i=2;i<=a;i++){
             if(a%i==0&&b%i==0){
                 a=a/i;
                 b=b/i;
-            }
+            }   
         }
-        
-        //b가 2와 5로만 이루어진 소인수여야 한다.\
-        //2만 5만 2,5만
-        for(int j=2;j<=b;j++){
-            if(b%j==0){
-                if(j%2!=0&&j%5!=0){
+        //이제 a/b 는 기약분수
+        //
+        int answer=0;
+        for(int i=2;i<=b;i++){
+            if(b%i==0){
+                //인수 일 때 2나 5
+                if(i%2!=0&&i%5!=0)
                     answer=2;
-                }
             }
         }
         return answer==2?2:1;
