8f9cc7071a4136724258a3dd5fba1a45f6acc024
형철 심 <sim34122@naver.com>
2023-10-16 10:08:03 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.16 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] seoul) {
+        int idx=0;
+        for(int i=0;i<seoul.length;i++){
+            if(seoul[i].equals("Kim"))
+                idx=i;
+        }
+        StringBuilder sb=new StringBuilder();
+        sb.append("김서방은 ").append(idx).append("에 있다");
+        return sb.toString();
+    }
+}
