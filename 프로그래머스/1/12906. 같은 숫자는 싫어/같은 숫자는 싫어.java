1f2c188e7b91cb638a50a84371fd9a6b60c1441c
형철 심 <sim34122@naver.com>
2023-10-24 10:18:43 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 28.51 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        Queue<Integer> q= new LinkedList<>();
+        //배열의 원소는 0~9
+        //연속적으로 나타나는 숫자는 하나만 남기고 제거
+        int tmp=-1;
+        for(int i:arr){
+            if(tmp==i)
+                continue;
+            q.offer(i);
+            tmp=i;
+        }
+        int[] answer=new int[q.size()];
+        int i=0;
+        while(!q.isEmpty()){
+            answer[i]=q.poll();
+            i++;
+        }       
+
+        return answer;
+    }
+}
