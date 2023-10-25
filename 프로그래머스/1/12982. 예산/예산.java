32c7d0bbfbfe5b1957aec3d786e91b78cacc1702
형철 심 <sim34122@naver.com>
2023-10-25 10:16:43 +0900

    [level 1] Title: 예산, Time: 0.49 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,16 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
-        //가장 많은 부서에 물품을 구매해주려면 오름차순으로 정렬하고 할당
-        Arrays.sort(d);
-        int answer=0;
-        for(int i:d){
-            budget-=i;
-            if(budget<0)
-                return answer;
-            answer++;
-        }
+        int answer = 0;
         return answer;
-        
     }
 }
