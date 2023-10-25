9452abc38ca9ef06f8e502e7c33a973bf1ba7779
형철 심 <sim34122@naver.com>
2023-10-25 15:34:57 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 9.28 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -8,14 +8,7 @@ class Solution {
             if(ch==' '){
                 idx=-1;
             }
-            //숫자일 경우
-                if(ch<='9'&&ch>='0') answer+=ch;
-                //숫자가 아닌데 첫 인덱스라 대문자로 바꿔야 할 경우
-                else if(idx==0){
-                answer+=Character.toUpperCase(ch);
-                }else{ //
-                    answer+=Character.toLowerCase(ch);
-                }
+            answer+=ch<='9'&&ch>='0'?ch:idx==0?Character.toUpperCase(ch):Character.toLowerCase(ch);
             idx++;
         }
         return answer;
