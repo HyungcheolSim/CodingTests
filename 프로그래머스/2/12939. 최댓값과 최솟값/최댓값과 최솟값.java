404fc7655618010f2f14129159ec0157b094c6db
형철 심 <sim34122@naver.com>
2023-10-25 15:09:27 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 11.97 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,13 +1,12 @@
 class Solution {
     public String solution(String s) {
         String[] arr=s.split(" ");
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
+        int min, max, n;
+        min = max = Integer.parseInt(arr[0]);
         for(int i=0;i<arr.length;i++){
-            if(Integer.parseInt(arr[i])>max)
-                max=Integer.parseInt(arr[i]);
-            if(Integer.parseInt(arr[i])<min)
-                min=Integer.parseInt(arr[i]);
+            n = Integer.parseInt(arr[i]);
+            if(n>max) max=n;
+            if(n<min) min=n;
         }
         return min+" "+max;
     }
