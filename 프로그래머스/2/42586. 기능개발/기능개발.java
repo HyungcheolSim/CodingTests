fdb9f18333a48ed9a696617ee647509b2e431ace
형철 심 <sim34122@naver.com>
2023-11-14 11:33:43 +0900

    [level 2] Title: 기능개발, Time: 2.32 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,22 +1,18 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> q=new LinkedList();
-        List<Integer> list=new ArrayList();
+        Queue<Integer> q=new LinkedList<>();
+        List<Integer> list=new ArrayList<>();
 
         for(int i=0;i<progresses.length;i++){
-            q.offer((int)Math.ceil((100-progresses[i])/(double)speeds[i]));
-        }
-        
-        while(!q.isEmpty()){
-            int day=q.poll();
-            int count=1;
-            while(!q.isEmpty()&&q.peek()<=day){
-                q.poll();
-                count++;
+            int date = (int)Math.ceil((100-progresses[i])/(double)speeds[i]);
+            if(!q.isEmpty() && q.peek() < date){
+                list.add(q.size());
+                q.clear();
             }
-            list.add(count);
+            q.offer(date);
         }
+        list.add(q.size());
 
         return list.stream().mapToInt(x->x).toArray();
     }
