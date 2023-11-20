e099eca34acd654d9df677d95e792a39d415eaab
형철 심 <sim34122@naver.com>
2023-11-20 17:02:34 +0900

    [level 2] Title: 가장 큰 수, Time: 2.06 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,16 +1,22 @@
-import java.util.Arrays;
+import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
         //못품!
         String[] arr= new String[numbers.length];
+        boolean isZero = true;
         
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<numbers.length;i++){
+            if(isZero && numbers[i] != 0){
+                isZero = false;
+            }
             arr[i]=String.valueOf(numbers[i]);
+        }
+        if(isZero){
+            return "0";
         }
         
         Arrays.sort(arr,(o1,o2)->(o2+o1).compareTo(o1+o2));
-        if(arr[0].equals("0"))
-            return "0";
+        
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<arr.length;i++){
             sb.append(arr[i]);
