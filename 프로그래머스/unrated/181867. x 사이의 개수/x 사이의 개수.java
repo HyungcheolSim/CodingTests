f6eecffb7e2340f3c163cefe911d8a622061fcb7
형철 심 <sim34122@naver.com>
2023-11-22 20:30:12 +0900

    [level unrated] Title: x 사이의 개수, Time: 2.94 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
@@ -4,14 +4,7 @@ class Solution {
         char ch=myString.charAt(myString.length()-1);
         int[] answer=new int[ch=='x'?strArr.length+1:strArr.length];
         for(int i=0;i<strArr.length;i++){
-            int len=strArr[i].length();
-            if(len>1){
-                answer[i]=len;
-            }
-            else if(len==1&&strArr[i].equals(" ")){
-                answer[i]=0;
-            }else
-                answer[i]=len;
+            answer[i]=strArr[i].length();
         }
         return answer;
     }
