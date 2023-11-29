46dffb3c1f50e242c36e3518fa46b087e288befd
형철 심 <sim34122@naver.com>
2023-11-29 21:44:51 +0900

    [level unrated] Title: 그림 확대, Time: 7.54 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        //k번 반복 추가, 원소의 내용도 하나씩 3번씩 추가
+        String[] answer= new String[picture.length*k];
+        int idx=0;
+        for(String s:picture){
+            StringBuilder sb=new StringBuilder();
+            for(int i=0;i<s.length();i++){
+                sb.append((s.charAt(i)+"").repeat(k));
+            }
+            int j=0;
+            while(j<k){
+                answer[idx]= sb.toString();
+                j++;
+                idx++;
+            }
+        }
+        return answer;
+    }
+}
