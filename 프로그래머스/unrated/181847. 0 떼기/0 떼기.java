f5b572b5593a095136cb74a7f5832c01e08c070f
형철 심 <sim34122@naver.com>
2023-11-29 20:40:25 +0900

    [level unrated] Title: 0 떼기, Time: 0.05 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,9 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        for(int i=0;i<n_str.length();i++){
-            if(n_str.charAt(i)!='0')
-               return n_str.substring(i);
-        }
-        return null;
+        return String.valueOf(Integer.parseInt(n_str));
     }
 }
