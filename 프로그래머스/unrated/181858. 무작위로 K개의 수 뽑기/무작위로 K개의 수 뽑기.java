258e2cb1dec279fcc268b4c171ca78ac92013af1
형철 심 <sim34122@naver.com>
2023-11-29 09:07:54 +0900

    [level unrated] Title: 무작위로 K개의 수 뽑기, Time: 11.05 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        ArrayList<Integer> list= new ArrayList();
+        
+        for(int i=0;i<arr.length;i++){
+            if(list.size()==k)
+                break;
+            if(!list.contains(arr[i]))
+                list.add(arr[i]);
+        }
+        while(list.size()<k){
+            list.add(-1);
+        }
+        int[] answer= new int[k];
+        for(int i=0;i<answer.length;i++){
+            answer[i]=list.get(i);
+        }
+        return answer;
+    }
+}
