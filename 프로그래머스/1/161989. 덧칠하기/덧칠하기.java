7ecd6b7c80b03f949ca466a8237e27968d18c560
형철 심 <sim34122@naver.com>
2023-12-07 08:52:00 +0900

    [level 1] Title: 덧칠하기, Time: 1.07 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,17 +1,14 @@
 import java.util.ArrayList;
 class Solution {
     public int solution(int n, int m, int[] section) {
-        //section의 원소들을 m 씩 나누어 제거
-        int count=1;
-        int start=section[0];
-        int end=section[0]+m-1;
-        for(int i=0;i<section.length;i++){
-            if(section[i]>end){
-                start=section[i];
-                end=section[i]+m-1;
-                count++;
+        int roller = section[0];
+        int cnt = 1;
+        for(int i = 1; i < section.length; i++) {
+            if(roller + m - 1 < section[i]) {
+                cnt++;
+                roller = section[i];
             }
         }
-        return count;
+        return cnt;
     }
 }
