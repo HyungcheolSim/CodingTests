ce863ed32ea4ed6ea1afb88245014f6e9c43f3f0
형철 심 <sim34122@naver.com>
2024-02-03 16:45:08 +0900

    [Bronze III] Title: 약수 구하기, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -1,11 +1,18 @@
-import java.util.*;
-public class Main{
-     public static void main(String[] args){
-         Scanner scan = new Scanner(System.in);
-         int N=scan.nextInt();
-         int order=scan.nextInt();
-         List<Integer> answer = new ArrayList();
-         for(int i=1;i<=Math.sqrt(N);i++){
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nums=br.readLine().split(" ");
+        int N = Integer.parseInt(nums[0]);
+        int order = Integer.parseInt(nums[1]);
+        ArrayList<Integer> answer = new ArrayList();
+        for(int i=1;i<=Math.sqrt(N);i++){
              if(N%i==0){
                  answer.add(i);
                  if(i!=N/i){
