e58a25e89d42b255a37971653b36be39cc81e1da
형철 심 <sim34122@naver.com>
2024-02-26 21:00:23 +0900

    [Bronze II] Title: 수 정렬하기, Time: 140 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,29 +1,21 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 public class Main{
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-         //저장
-        int[] arr = new int[N];
-        for(int i=0;i<N;i++){
-            arr[i]=Integer.parseInt(br.readLine());
-        }
-        // 정렬
-        Arrays.sort(arr);
-        // 출력
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.flush();
-        for(int n:arr){
-            bw.write(n+"");
-            bw.newLine();
-        }
-        br.close();
-        bw.flush();
-        bw.close();
-    }
+        boolean[] arr = new boolean[2001];
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++) {
+			arr[Integer.parseInt(br.readLine()) + 1000] = true;
+		}
+		
+		for(int i = 0; i < 2001; i++) {
+			if(arr[i]) {
+				sb.append(i - 1000).append('\n');
+			}
+		}
+		System.out.println(sb);
+	}
 }
