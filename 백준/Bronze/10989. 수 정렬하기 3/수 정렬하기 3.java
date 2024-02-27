b7816eb5ff1964f23be119b9727cb9fa15873d75
형철 심 <sim34122@naver.com>
2024-02-27 21:14:30 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1372 ms, Memory: 301616 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,25 +1,26 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        //나올 수 있는 숫자의 범위+1 의 크기를 갖는 배열
         int[] cnt = new int[10001];
-
         int N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
-            cnt[Integer.parseInt(br.readLine())] ++;
+
+        for(int i=0;i<N;i++){
+            cnt[Integer.parseInt(br.readLine())]++;
         }
-				
         br.close();
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; i < 10001; i++){
-            while(cnt[i] > 0){
-                sb.append(i).append('\n');
-                cnt[i]--;
-            }
+       
+        for(int i=1;i< 10001;i++){ 
+            //값이 없을 때 스킵하기
+            if(cnt[i]==0) continue;
+            //repeat로 반복문 제거
+            bw.write((i+"\n").repeat(cnt[i]));
         }
-        System.out.println(sb);
-	}
+        bw.flush();
+        bw.close();
+    }
 }
