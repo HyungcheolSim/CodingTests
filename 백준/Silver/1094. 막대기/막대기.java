d5a10b8e6be876b7488c0022be32a0612e028509
형철 심 <sim34122@gmail.com>
2024-10-21 22:38:36 +0900

    [Silver V] Title: 막대기, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(Integer.toBinaryString(n).replace("0","").length());
+        br.close();
+    }
+}
