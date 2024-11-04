fe202b4ad4af80306507a55919842c20c9701510
형철 심 <sim34122@gmail.com>
2024-11-05 00:33:54 +0900

    [Bronze V] Title: A+B - 4, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String str;
+        while((str=br.readLine()) != null){
+            st = new StringTokenizer(str," ");
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+            sb.append(num1+num2).append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
