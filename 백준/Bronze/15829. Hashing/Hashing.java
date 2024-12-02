605ebc01398eb54e438dcec71801cbf5ca243b0a
형철 심 <sim34122@gmail.com>
2024-12-02 22:36:04 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14324 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -7,12 +7,14 @@ public class Main {
         long answer = 0;
         int seq = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        br.readLine();
-        for (char ch : br.readLine().toCharArray()) {
-            int i = ch - 'a';
-            i++;
-            answer += (i * (long) (Math.pow(31, seq++)));
-        }
-        System.out.print(answer);
-    }
+		int L = Integer.parseInt(br.readLine());
+		String S = br.readLine();
+		long result = 0;
+		long pow = 1;
+		for(int i = 0; i < L; i++) {
+			result += ((S.charAt(i) - 96) * pow);
+			pow = (pow * 31) % 1234567891;
+		}
+		System.out.println(result % 1234567891);
+	}
 }
