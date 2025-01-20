b2efced04d8c83eef55881efc83ff24e27cd9848
형철 심 <sim34122@gmail.com>
2025-01-20 23:27:32 +0900

    [Silver III] Title: 통계학, Time: 288 ms, Memory: 39972 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -1,63 +1,60 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
+import java.io.IOException;
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        print(getArithmeticMean(arr));
-        print(getMedian(arr));
-        print(getMode(arr));
-        print(getRange(arr));
-    }
-    private static void print(int i) {
-        System.out.println(i);
-    }
-    private static int getArithmeticMean(int[] arr) {
-        double sum = 0;
-        for (int n : arr) {
-            sum += n;
-        }
-        return (int) Math.round(sum/arr.length);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[8001];
 
-    private static int getMedian(int[] arr) {
-        Arrays.sort(arr);
-        return arr[ arr.length/ 2];
-    }
+		int sum = 0;
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
 
-    private static int getMode(int[] arr) {
-        if (arr.length == 1) {
-            return arr[0];
-        }
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int key : arr) {
-            map.put(key, map.getOrDefault(key, 0) + 1);
-        }
-        int max = Collections.max(map.values());
+		int median = 10000;
+		int mode = 10000;
+		
+		for(int i = 0; i < N; i++) {
+			int value = Integer.parseInt(br.readLine());
+			sum += value;
+			arr[value + 4000]++;
+		
+			if(max < value) {
+				max = value;
+			}
+			if(min > value) {
+				min = value;
+			}
+		}
+		int count = 0;
+		int mode_max = 0; 
 
-        List<Integer> answers = new ArrayList<>();
-        for (int key : map.keySet()) {
-            if (map.get(key) == max) {
-                answers.add(key);
-            }
-        }
-        if (answers.size() > 1) {
-            Collections.sort(answers);
-            return answers.get(1);
-        }else{
-            return answers.get(0);
-        }
-        
-    }
-    private static int getRange(int[] arr) {
-        Arrays.sort(arr);
-        return Math.abs(arr[arr.length - 1] - arr[0]);
-    }
+		boolean flag = false;	 
+		
+		for(int i = min + 4000; i <= max + 4000; i++) {
+			if(arr[i] > 0) {
+				if(count < (N + 1) / 2) {
+					count += arr[i];	
+					median = i - 4000;
+				}
+
+				if(mode_max < arr[i]) {
+					mode_max = arr[i];
+					mode = i - 4000;
+					flag = true; 
+				}
+				else if(mode_max == arr[i] && flag == true) {
+					mode = i - 4000;
+					flag = false;					
+				}
+			}
+		}
+		
+		System.out.println((int)Math.round((double)sum / N));
+		System.out.println(median);	
+		System.out.println(mode);	
+		System.out.println(max - min);
+	}
+ 
 }
