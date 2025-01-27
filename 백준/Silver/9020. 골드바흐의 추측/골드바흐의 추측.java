3427342b439e5645849aabfd1ebad6ecf6c2a3a7
형철 심 <sim34122@gmail.com>
2025-01-27 22:21:00 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 176 ms, Memory: 16976 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,34 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        int A = 0, B = 0;
-        for (int i = 0; i < T; i++) {
-            int n = Integer.parseInt(br.readLine());
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static int T;
+    static boolean[] arr;
 
-            for (int p = 2; p <= (n / 2); p++) {
-                if (isPrime(p) && isPrime(n - p)) {
-                    A = p;
-                    B = n - p;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        T = Integer.parseInt(br.readLine());
+        getPrime(10001);
+
+        while (T-- > 0) {
+            int cur = Integer.parseInt(br.readLine());
+            for (int i = cur / 2; i >= 2; i--) {
+                if (!arr[i] && !arr[cur - i]) {
+                    bw.write(i + " " + (cur - i) + "\n");
+                    break;
                 }
             }
-            System.out.println(A + " " + B);
         }
+        bw.flush();
+        bw.close();
     }
 
-    private static boolean isPrime(int num) {
-        if (num < 2) return false;
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false;
+
+    public static void getPrime(int size) {
+        arr = new boolean[size];
+        arr[0] = arr[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(size); i++) {
+            if (arr[i]) continue;
+            for (int j = i * i; j < size; j += i) {
+                arr[j] = true;
             }
         }
-        return true;
     }
 }
