3a5f7d1cc2ecdc73b4b1b0e8e4cdd501d30168d5
형철 심 <sim34122@gmail.com>
2025-01-28 12:22:03 +0900

    [Silver IV] Title: 30, Time: 164 ms, Memory: 16572 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -1,40 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     static BufferedReader br;
-
-    public static String str;
-    public static int[] numCountArr;
-    public static long strLen;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
 
-        str = br.readLine();
-        strLen = str.length();
-
-        numCountArr = new int[10]; //각 숫자 별 나타난 횟수 저장
-        long total = 0; //모든 자리수의 합이 3의 배수면 30의 배수다
-        for (int i = 0; i < strLen; i++) {
-            int tNum = Integer.parseInt(str.substring(i, i + 1));
-            //int tNum = str.charAt(i)-'0';
-            numCountArr[tNum] += 1;
-            total += tNum;
+        char[] arr = br.readLine().toCharArray();
+        Arrays.sort(arr);
+        int sum = 0;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int num = arr[i] - '0';
+            sum += num;
+            sb.append(num);
         }
 
-        if (!str.contains("0") || total % 3 != 0) {
-            System.out.println("-1");
+        if (sum % 3 != 0 || arr[0] != '0') {
+            System.out.println(-1);
             return;
         }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 9; i >= 0; i--) {
-            while (numCountArr[i] > 0) {
-                sb.append(i);
-                numCountArr[i]--;
-            }
-        }
+
         System.out.println(sb);
     }
 }
