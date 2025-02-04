23604a3742692e06e3cb0dbf9919004cecef8aef
형철 심 <sim34122@gmail.com>
2025-02-04 21:37:29 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 516 ms, Memory: 54352 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -7,21 +7,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Map<String, String> map = new HashMap<>(N);
+        Map<String, String> map = new HashMap<>((int) (N / 0.75) + 1);
+        
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             map.put(st.nextToken(), st.nextToken());
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
-            String url = br.readLine();
-            bw.write(map.get(url) + "\n");
+            sb.append(map.get(br.readLine())).append("\n");
         }
+
+        bw.write(sb.toString());
 
         bw.flush();
         bw.close();
