76e91e40e72872fbf4d976376f85461d1a9eeb01
형철 심 <sim34122@gmail.com>
2025-02-06 21:29:00 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,49 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int count, num, connections;
-    static boolean[] visited;
-    static List[] computers;
+    private static List<Integer>[] graph;
+    private static boolean chk[];
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        num = Integer.parseInt(br.readLine());
-        connections = Integer.parseInt(br.readLine());
-        visited = new boolean[num + 1];
-        computers = new List[num + 1];
-        count = 0;
-        for (int i = 1; i < num + 1; i++) {
-            computers[i] = new ArrayList<Integer>();
-        }
 
-        StringTokenizer st;
-        for (int i = 0; i < connections; i++) {
-            st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(br.readLine());
+        int e = Integer.parseInt(br.readLine());
+
+        graph = new ArrayList[v + 1];
+        for (int i = 0; i <= v; i++) graph[i] = new ArrayList<>();
+        chk = new boolean[v + 1];
+
+        while (e-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            computers[a].add(b);
-            computers[b].add(a);
+
+            graph[a].add(b);
+            graph[b].add(a);
         }
 
-        dfs(1);
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        chk[1] = true;
+        int cnt = 0;
+        while (!q.isEmpty()) {
+            int poll = q.poll();
 
-        System.out.println(count - 1); // 시작 지점 카운트 빼주기
-        br.close();
-    }
-
-    private static void dfs(int start) {
-        if (!visited[start]) {
-            visited[start] = true;
-            count++;
-            for (int i = 0; i < computers[start].size(); i++) {
-                dfs((int) computers[start].get(i));
+            for (int i : graph[poll]) {
+                if (!chk[i]) {
+                    chk[i] = true;
+                    cnt++;
+                    q.add(i);
+                }
             }
         }
+        System.out.println(cnt);
     }
 }
-
