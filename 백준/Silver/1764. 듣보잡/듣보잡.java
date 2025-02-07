4813189bf4a3cb30a8c743029909bb361e60724b
형철 심 <sim34122@gmail.com>
2025-02-07 23:55:17 +0900

    [Silver IV] Title: 듣보잡, Time: 284 ms, Memory: 26564 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -4,34 +4,30 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        Map<String,Integer> map = new HashMap<>();
-        for(int i=0;i<n+m;i++){
-            String name = br.readLine();
-            if(map.containsKey(name)){
-                map.put(name,map.get(name)+1);
-            }else{
-                map.put(name,1);
-            }
-        }
-        Map<String, Integer> result = new HashMap<>();
 
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == 2) {
-                result.put(entry.getKey(), entry.getValue());
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        TreeSet<String> resultSet = new TreeSet<>();
+        for (int i = 0; i < m; i++) {
+            String str = br.readLine();
+            if (set.contains(str)) {
+                resultSet.add(str);
             }
         }
-        StringBuilder sb = new StringBuilder();
-        int size =result.size();
-        sb.append(size).append("\n");
-        List<String> answerArr = new ArrayList<>(result.keySet());
-        Collections.sort(answerArr);
-        for(String name:answerArr){
-            sb.append(name).append("\n");
+
+        bw.write(resultSet.size() + "\n");
+        for (String str : resultSet) {
+            bw.write(str + "\n");
         }
-        System.out.println(sb);
+        bw.flush();
+        bw.close();
     }
 }
