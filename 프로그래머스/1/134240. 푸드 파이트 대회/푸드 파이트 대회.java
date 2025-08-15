e3393137e695041272715cc6cac31cf230b7b79a
형철 심 <sim34122@gmail.com>
2025-08-15 14:44:09 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.44 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,14 +1,9 @@
 class Solution {
     public String solution(int[] food) {
-        StringBuilder sb = new StringBuilder("0");
-		for (int i = food.length-1; i >0; i--) {
-			int amount = food[i];
-			while (amount > 1) {
-				sb.insert(0, i);
-				sb.append(i);
-				amount-=2;
-			}
-		}
-		return sb.toString();
-	}
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < food.length; i++) {
+            sb.append(Integer.toString(i).repeat(food[i] / 2));
+        }
+        return sb.toString() + "0" + sb.reverse().toString();
+    }
 }
